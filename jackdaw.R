8c5bc06912f2b1be477c780517d0051a56de3a48
ArchieArchieArchie <166266940+ArchieArchieArchie@users.noreply.github.com>
2024-04-08 10:32:56 +0100

    Update jackdaw.R

--- jackdaw.R
+++ jackdaw.R
@@ -547,3 +547,92 @@ summary(c1)
 # male invesetment
 round(mean(Jx$food.share.p),3)
 round(sd(Jx$food.share.p),3)
+
+# heatmap
+install.packages("ggplot2")
+library("ggplot2")
+library(reshape2)
+H <-read.table("heatmap.txt",header = T)
+H1 <-read.table("heatmap1.txt",header = T)
+
+df <- na.omit(H1)
+
+cor_df <- round(cor(as.matrix(df)), 2)
+
+melted_cormat <- melt(cor_df)
+
+ggplot(data = melted_cormat, aes(x=Var1, y=Var2, fill=value)) + 
+  geom_tile() +
+  geom_text(aes(Var2, Var1, label = value), size = 5) +
+  scale_fill_gradient2(low = "#3F88C5", high = "#D00000",
+                       limit = c(-1,1), name="Correlation") +
+  theme(axis.title.x = element_blank(),
+        axis.title.y = element_blank(),
+        axis.text.x = element_text(angle = 45, vjust = 1, hjust=1),
+        panel.background = element_blank())
+
+# Get lower triangle of the correlation matrix
+get_lower_tri<-function(cor_df){
+  cor_df[upper.tri(cor_df)] <- NA
+  return(cor_df)
+}
+# Get upper triangle of the correlation matrix
+get_upper_tri <- function(cor_df){
+  cor_df[lower.tri(cor_df)]<- NA
+  return(cor_df)
+}
+
+upper_tri <- get_upper_tri(cor_df)
+
+melted_cormat <- melt(upper_tri, na.rm = TRUE)
+
+ggplot(data = melted_cormat, aes(Var2, Var1, fill = value))+
+  geom_tile(color = "white")+
+  scale_fill_gradient2(low = "blue", high = "red", mid = "white", 
+                       midpoint = 0, limit = c(-1,1), space = "Lab", 
+                       name="Pearson\nCorrelation") +
+  theme_minimal()+ 
+  theme(axis.text.x = element_text(angle = 45, vjust = 1, 
+                                   size = 12, hjust = 1))+
+  coord_fixed()
+
+reorder_cor_df <- function(cor_df){
+  # Use correlation between variables as distance
+  dd <- as.dist((1-cor_df)/2)
+  hc <- hclust(dd)
+  cor_df <-cor_df[hc$order, hc$order]
+}
+
+cor_df <- reorder_cor_df(cor_df)
+upper_tri <- get_upper_tri(cor_df)
+
+melted_cormat <- melt(upper_tri, na.rm = TRUE)
+
+ggheatmap <- ggplot(melted_cormat, aes(Var2, Var1, fill = value))+
+  geom_tile(color = "white")+
+  scale_fill_gradient2(low = "blue", high = "red", mid = "white", 
+                       midpoint = 0, limit = c(-1,1), space = "Lab", 
+                       name="Pearson\nCorrelation") +
+  theme_minimal()+ # minimal theme
+  theme(axis.text.x = element_text(angle = 70, vjust = 1, 
+                                   size = 8, hjust = 1))+
+  coord_fixed()
+print(ggheatmap)
+
+ggheatmap + 
+  geom_text(aes(Var2, Var1, label = value), color = "black", size = 1.8,fontface = "bold") +
+  theme(
+    axis.title.x = element_blank(),
+    axis.title.y = element_blank(),
+    panel.grid.major = element_blank(),
+    panel.border = element_blank(),
+    panel.background = element_blank(),
+    axis.ticks = element_blank(),
+    legend.justification = c(1, 0),
+    legend.position = c(0.6, 0.7),
+    legend.direction = "horizontal")+
+  guides(fill = guide_colorbar(barwidth = 7, barheight = 1,
+                               title.position = "top", title.hjust = 0.5))
+
++ theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+
